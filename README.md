2dd54a8b3262637c7da267aae05975c55e596ae6
Berdnik <ggwpez1987@mail.ru>
2023-06-16 15:39:38 +0700

    added txt test files

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/nikitaChandler01/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/nikitaChandler01/frontend-project-46/actions)
-<a href="https://codeclimate.com/github/nikitaChandler01/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/60a7275435fbac4e84ff/maintainability" /></a>
+[![Maintainability](https://api.codeclimate.com/v1/badges/090881c618bd586b55a5/maintainability)](https://codeclimate.com/github/nikitaChandler01/frontend-project-46/maintainability)
 
 ### CodeClimate test-coverage status:  
 [![Test Coverage](https://api.codeclimate.com/v1/badges/090881c618bd586b55a5/test_coverage)](https://codeclimate.com/github/nikitaChandler01/frontend-project-46/test_coverage)
--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -1,23 +1,24 @@
+import * as fs from 'node:fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname);
+console.log(__filename);
+const makePathToTestFile = (filename) => path.resolve(__dirname, '__fixtures__', filename);
+const makePathToJsonFile = (filename) => path.resolve(__dirname, '__fixtures__', filename);
 
-const pathToJsonFile1 = path.join(__dirname, '__fixtures__', 'file1.test.json');
-const pathToJsonFile2 = path.join(__dirname, '__fixtures__', 'file2.test.json');
-const pathToJsonFile3 = path.join(__dirname, '__fixtures__', 'file3.test.json');
-
-const actual12 = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
-const actual31 = '{\n  - age: 22\n  - blue-eyes: true\n    host: hexlet.io\n  - name: Nikita\n  - sense of humor: high-level\n  + follow: false\n  + proxy: 123.234.53.22\n  + timeout: 50\n}';
-const actual23 = '{\n    host: hexlet.io\n  - timeout: 20\n  - verbose: true\n  + age: 22\n  + blue-eyes: true\n  + name: Nikita\n  + sense of humor: high-level\n}';
 test('getDiff12', () => {
-  expect(getDiff(pathToJsonFile1, pathToJsonFile2)).toBe(actual12);
+  const actual12 = fs.readFileSync(makePathToTestFile('compare12.txt'), 'utf-8');
+  expect(getDiff(makePathToJsonFile('file1.test.json'), makePathToJsonFile('file2.test.json'))).toBe(actual12);
 });
 test('getDiff31', () => {
-  expect(getDiff(pathToJsonFile3, pathToJsonFile1)).toBe(actual31);
+  const actual31 = fs.readFileSync(makePathToTestFile('compare31.txt'), 'utf-8');
+  expect(getDiff(makePathToJsonFile('file3.test.json'), makePathToJsonFile('file1.test.json'))).toBe(actual31);
 });
 test('getDiff23', () => {
-  expect(getDiff(pathToJsonFile2, pathToJsonFile3)).toBe(actual23);
+  const actual23 = fs.readFileSync(makePathToTestFile('compare23.txt'), 'utf-8');
+  expect(getDiff(makePathToJsonFile('file2.test.json'), makePathToJsonFile('file3.test.json'))).toBe(actual23);
 });
