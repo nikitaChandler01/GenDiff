9b13690007295b574aa2b3f63ef644062a5cabb8
Berdnik <ggwpez1987@mail.ru>
2023-06-17 16:15:57 +0700

    tests for yaml files compare

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -5,20 +5,38 @@ import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-console.log(__dirname);
-console.log(__filename);
 const makePathToTestFile = (filename) => path.resolve(__dirname, '__fixtures__', filename);
-const makePathToJsonFile = (filename) => path.resolve(__dirname, '__fixtures__', filename);
 
-test('getDiff12', () => {
-  const actual12 = fs.readFileSync(makePathToTestFile('compare12.txt'), 'utf-8');
-  expect(getDiff(makePathToJsonFile('file1.test.json'), makePathToJsonFile('file2.test.json'))).toBe(actual12);
+describe('testJsonFiles', () => {
+  const makePathToJsonFile = (filename) => path.resolve(__dirname, '__fixtures__', 'jsonFiles', filename);
+
+  test('getDiff12', () => {
+    const actual12 = fs.readFileSync(makePathToTestFile('compare12.txt'), 'utf-8');
+    expect(getDiff(makePathToJsonFile('file1.test.json'), makePathToJsonFile('file2.test.json'))).toBe(actual12);
+  });
+  test('getDiff31', () => {
+    const actual31 = fs.readFileSync(makePathToTestFile('compare31.txt'), 'utf-8');
+    expect(getDiff(makePathToJsonFile('file3.test.json'), makePathToJsonFile('file1.test.json'))).toBe(actual31);
+  });
+  test('getDiff23', () => {
+    const actual23 = fs.readFileSync(makePathToTestFile('compare23.txt'), 'utf-8');
+    expect(getDiff(makePathToJsonFile('file2.test.json'), makePathToJsonFile('file3.test.json'))).toBe(actual23);
+  });
 });
-test('getDiff31', () => {
-  const actual31 = fs.readFileSync(makePathToTestFile('compare31.txt'), 'utf-8');
-  expect(getDiff(makePathToJsonFile('file3.test.json'), makePathToJsonFile('file1.test.json'))).toBe(actual31);
-});
-test('getDiff23', () => {
-  const actual23 = fs.readFileSync(makePathToTestFile('compare23.txt'), 'utf-8');
-  expect(getDiff(makePathToJsonFile('file2.test.json'), makePathToJsonFile('file3.test.json'))).toBe(actual23);
+
+describe('testYamlFiles', () => {
+  const makePathToYamlFile = (filename) => path.resolve(__dirname, '__fixtures__', 'yamlFiles', filename);
+
+  test('getDiff12', () => {
+    const actual12 = fs.readFileSync(makePathToTestFile('compare12.txt'), 'utf-8');
+    expect(getDiff(makePathToYamlFile('file1.test.yml'), makePathToYamlFile('file2.test.yml'))).toBe(actual12);
+  });
+  test('getDiff31', () => {
+    const actual31 = fs.readFileSync(makePathToTestFile('compare31.txt'), 'utf-8');
+    expect(getDiff(makePathToYamlFile('file3.test.yml'), makePathToYamlFile('file1.test.yml'))).toBe(actual31);
+  });
+  test('getDiff23', () => {
+    const actual23 = fs.readFileSync(makePathToTestFile('compare23.txt'), 'utf-8');
+    expect(getDiff(makePathToYamlFile('file2.test.yml'), makePathToYamlFile('file3.test.yml'))).toBe(actual23);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import parseJsonFile from './parseJsonFile.js';
+import parseJsonFile from './parsers/parseJsonFile.js';
+import parseYamlFile from './parsers/parseYamlFile.js';
 import buildDiff from './buildDiff/buildDiff.js';
 
 const getDiff = (filepath1, filepath2) => {
@@ -8,8 +9,8 @@ const getDiff = (filepath1, filepath2) => {
   const __dirname = path.dirname(__filename);
   const absolutePath1 = path.resolve(__dirname, filepath1);
   const absolutePath2 = path.resolve(__dirname, filepath2);
-  const dataParse1 = parseJsonFile(absolutePath1);
-  const dataParse2 = parseJsonFile(absolutePath2);
+  const dataParse1 = (path.extname(absolutePath1) === '.json') ? parseJsonFile(absolutePath1) : parseYamlFile(absolutePath1);
+  const dataParse2 = (path.extname(absolutePath2) === '.json') ? parseJsonFile(absolutePath2) : parseYamlFile(absolutePath2);
   return buildDiff(dataParse1, dataParse2);
 };
 
