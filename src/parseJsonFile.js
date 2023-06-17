31819f9dc3968e6cec1f6bd094c184fa4018374c
Berdnik <ggwpez1987@mail.ru>
2023-06-17 16:26:06 +0700

    json and yaml files for tests orginized

--- src/parseJsonFile.js
+++
@@ -1,9 +0,0 @@
-import * as fs from 'node:fs';
-
-const parseJsonFile = (filepath) => {
-  const readFile = fs.readFileSync(filepath, 'utf-8');
-  const jsonFile = JSON.parse(readFile);
-  return jsonFile;
-};
-
-export default parseJsonFile;
