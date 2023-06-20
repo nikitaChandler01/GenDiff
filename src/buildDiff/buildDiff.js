2f19a7090107ff0e3e80b49ed4b5ea6e41df14ee
Berdnik <ggwpez1987@mail.ru>
2023-06-20 15:39:25 +0700

    fucn can compare nested structures

--- src/buildDiff/buildDiff.js
+++ src/buildDiff/buildDiff.js
@@ -1,35 +1,66 @@
 import _ from 'lodash';
 import formaterOfDif from './stylish.js';
 import compareFunc from './compareFunc.js';
-import isSameKey from './isSameKey.js';
-import isSameValue from './isSameValue.js';
-import getValueByKey from './getValue.js';
 
 const buildDiff = (jsonFile1, jsonFile2) => {
   const jsObj1 = jsonFile1;
   const jsObj2 = jsonFile2;
-  let result = '';
+  let result = '{\n';
 
   const findDiff = (obj1, obj2, counter, acc) => {
-    result = `${result}${acc}\n`;
-    const entriesSorted1 = Object.entries(obj1).sort(compareFunc);
-    const entriesSorted2 = Object.entries(obj2).sort(compareFunc);
-    entriesSorted1.forEach((item) => {
-      console.log(result);
-      const [key, value] = item;
-      if (isSameKey(key, entriesSorted2)) {
-        const valueOfentriesSorted2 = getValueByKey(key, entriesSorted2);
-        if (typeof value === 'object' && typeof valueOfentriesSorted2 === 'object') {
-          findDiff(value, valueOfentriesSorted2, counter + 1, result);
+    const keys1 = Object.keys(obj1).sort(compareFunc);
+    const keys2 = Object.keys(obj2).sort(compareFunc);
+    const allKeys = _.union(keys1, keys2).sort();
+    allKeys.forEach((item) => {
+      if (keys1.includes(item) && keys2.includes(item)) {
+        if (typeof obj1[item] === 'object' && typeof obj2[item] === 'object') {
+          acc += findDiff(obj1[item], obj2[item], counter + 2, `${formaterOfDif(' ', counter)} ${item}: {\n`);
+          acc += `${formaterOfDif('}', counter + 1)}\n`;
+        } else if (typeof obj2[item] !== 'object' && typeof obj1[item] === 'object' && obj1[item] !== null) {
+          acc += findDiff(obj1[item], obj1[item], counter + 2, `${formaterOfDif('-', counter)} ${item}: {\n`);
+          acc += `${formaterOfDif('}', counter + 1)}\n`;
+          acc += `${formaterOfDif('+', counter)} ${item}: ${obj2[item]}\n`;
+        } else if (typeof obj1[item] !== 'object' && typeof obj2[item] === 'object' && obj2[item] !== null) {
+          acc += findDiff(obj2[item], obj2[item], counter + 2, `${formaterOfDif('+', counter)} ${item}: {\n`);
+          acc += `${formaterOfDif('}', counter + 1)}\n`;
+          acc += `${formaterOfDif('-', counter)} ${item}: ${obj1[item]}\n`;
+        } else if (obj1[item] === obj2[item]) {
+          acc += `${formaterOfDif(' ', counter)} ${item}: ${obj1[item]}\n`;
+        } else if (obj1[item] !== obj2[item]) {
+          acc += `${formaterOfDif('-', counter)} ${item}: ${obj1[item]}\n`;
+          acc += `${formaterOfDif('+', counter)} ${item}: ${obj2[item]}\n`;
         }
-        result = `${result}${formaterOfDif('+', counter)}${key}:${value}\n`;
-        result = `${result}${formaterOfDif('-', counter)}${key}:${valueOfentriesSorted2}\n`;
+      } else if (!keys2.includes(item)) {
+        if (typeof obj1[item] !== 'object') {
+          acc += `${formaterOfDif('-', counter)} ${item}: ${obj1[item]}\n`;
+        } else {
+          acc += findDiff(obj1[item], obj1[item], counter + 2, `${formaterOfDif('-', counter)} ${item}: {\n`);
+          acc += `${formaterOfDif('}', counter + 1)}\n`;
+        }
+      } else if (!keys1.includes(item)) {
+        if (typeof obj2[item] !== 'object') {
+          acc += `${formaterOfDif('+', counter)} ${item}: ${obj2[item]}\n`;
+        } else {
+          acc += findDiff(obj2[item], obj2[item], counter + 2, `${formaterOfDif('+', counter)} ${item}: {\n`);
+          acc += `${formaterOfDif('}', counter + 1)}\n`;
+        }
       }
-      result = `${result}${formaterOfDif('+', counter)}${key}:${value}\n`;
+      // if (isSameKey(key, entriesSorted2)) {
+      //   const valueOfentriesSorted2 = getValueByKey(key, entriesSorted2);
+      //   if (typeof value === 'object' && typeof valueOfentriesSorted2 === 'object') {
+      //     findDiff(value, valueOfentriesSorted2, counter + 1, result);
+      //   }
+      //   if ()
+      //   result = `${result}${formaterOfDif('+', counter)}${key}:${value}\n`;
+      //   result = `${result}${formaterOfDif('-', counter)}${key}:${valueOfentriesSorted2}\n`;
+      // }
+      // result = `${result}${formaterOfDif('+', counter)}${key}:${value}\n`;
     });
+    return acc;
   };
-  findDiff(jsObj1, jsObj2, 1, result);
+  result = `${findDiff(jsObj1, jsObj2, 1, result)}}`;
   console.log(result);
+  return result;
 };
 
 export default buildDiff;
--- src/buildDiff/isSameValue.js
+++
@@ -1,7 +0,0 @@
-const isSameValue = (value, array) => {
-  const [, sameValueOfArray] = array;
-  if (JSON.stringify(value) === JSON.stringify(sameValueOfArray)) return true;
-  return false;
-};
-
-export default isSameValue;
