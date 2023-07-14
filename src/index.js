b48be431d33c7856913e689fb6c7541baa2a5ceb
Berdnik <ggwpez1987@mail.ru>
2023-07-14 16:01:35 +0700

    parsers code refactored

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import parseFile from './parser.js';
+import parse from './parser.js';
 import buildDiff from './buildDiff.js';
 import formatDiff from './formatters/index.js';
 
@@ -9,8 +9,8 @@ const getDiff = (filepath1, filepath2, formatterName) => {
   const __dirname = path.dirname(__filename);
   const absolutePath1 = path.resolve(__dirname, '../__fixtures__', filepath1);
   const absolutePath2 = path.resolve(__dirname, '../__fixtures__', filepath2);
-  const dataParse1 = parseFile(absolutePath1);
-  const dataParse2 = parseFile(absolutePath2);
+  const dataParse1 = parse(absolutePath1);
+  const dataParse2 = parse(absolutePath2);
   const tree = buildDiff(dataParse1, dataParse2);
   return formatDiff(tree, formatterName);
 };
--- src/parser.js
+++ src/parser.js
@@ -2,16 +2,28 @@ import yaml from 'js-yaml';
 import path from 'path';
 import fs from 'node:fs';
 
-const parseFile = (filepath) => {
-  if (path.extname(filepath) === ('.yml' || '.yaml')) {
-    const readFileSync = fs.readFileSync(filepath, 'utf-8');
-    return yaml.load(readFileSync);
-  }
-  if (path.extname(filepath) === '.json') {
-    const readFileSync = fs.readFileSync(filepath, 'utf-8');
-    return JSON.parse(readFileSync);
-  }
-  return console.log('Error! Unknown file extension!');
+const getFileExtname = (filepath) => path.extname(filepath).slice(1);
+
+const parse = (filepath) => {
+  const readFileSync = fs.readFileSync(filepath, 'utf-8');
+  const parsers = {
+    json: JSON.parse,
+    yaml: yaml.load,
+    yml: yaml.load,
+  };
+  return parsers[getFileExtname(filepath)](readFileSync);
 };
 
-export default parseFile;
+export default parse;
+
+// const parsers = (filepath) => {
+//   if (path.extname(filepath) === ('.yml' || '.yaml')) {
+//     const readFileSync = fs.readFileSync(filepath, 'utf-8');
+//     return yaml.load(readFileSync);
+//   }
+//   if (path.extname(filepath) === '.json') {
+//     const readFileSync = fs.readFileSync(filepath, 'utf-8');
+//     return JSON.parse(readFileSync);
+//   }
+//   return console.log('Error! Unknown file extension!');
+// };
